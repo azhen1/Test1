983f268d25c030519f95a4b9e0a794e4c1f747c0
lp 123456 <529939541@qq.com>
2018-01-09 11:06:19 +0800

    update .gitignore

---
+++ build/serviceConfig.js
@@ -0,0 +1,17 @@
+var os = require('os'),
+    iptable = {},
+    ifaces = os.networkInterfaces();
+for (var dev in ifaces) {
+    ifaces[dev].forEach(function (details, alias) {
+        console.log(details, '......')
+        if (details.family=='IPv4') {
+            iptable[dev] = details.address;
+        }
+    });
+}
+var serviceList = {
+    port: 8080,
+    host: '0.0.0.1'
+}
+
+module.exports = serviceList
